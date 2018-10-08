b42bc7bb5cae9f4deeaf6f9f5fb4dd9628f732b9
mike <brendoamaro@gmail.com>
2018-10-08 20:05:51 -0300

    Projeto

--- organizador/views.py
+++ organizador/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import Organizador
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 class OrganizadorCreateView(CreateView):
     model = Organizador
     fields = {'nome', 'descricao'}
+    success_url = reverse_lazy('org_list')
 
 class OrganizadorListView(ListView):
     model = Organizador
@@ -12,6 +14,8 @@ class OrganizadorListView(ListView):
 
 class OrganizadorUpdateView(UpdateView):
     model = Organizador
+    success_url = reverse_lazy('org_list')
 
 class OrganizadorDeleteView(DeleteView):
     model = Organizador
+    success_url = reverse_lazy('org_list')
